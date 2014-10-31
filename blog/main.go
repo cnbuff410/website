88b59c757282e32a14c55159500fffa3ff9f4fce
Kun <likunarmstrong@gmail.com>
2014-10-30 23:54:39 -0600

    Serve content through memcache. Close #3

--- blog/main.go
+++ blog/main.go
@@ -11,11 +11,14 @@ import (
 	"github.com/gorilla/mux"
 	"appengine"
 	"appengine/datastore"
+	"appengine/memcache"
 )
 
 const (
-	pathPrefix string = "static/posts"
-	postKind          = "post_meta"
+	pathPrefix         = "static/posts"
+	postKind           = "post_meta"
+	allPostKey         = "allPost"
+	postContentKeyBase = "post_"
 )
 
 func init() {
@@ -35,14 +38,10 @@ func init() {
 }
 
 func blogServeHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	c.Infof("In blog serve")
 	http.ServeFile(w, r, "./web/blog.html")
 }
 
 func postServeHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	c.Infof("In post serve")
 	http.ServeFile(w, r, "./web/post.html")
 }
 
@@ -62,36 +61,97 @@ func postUpdateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Update complete: %v posts", len(posts))
+
+	// Update memcache
+	value, err := json.Marshal(posts)
+	if err != nil {
+		c.Errorf("error marshal posts: %v", err)
+		return
+	}
+	item := &memcache.Item{
+		Key:   allPostKey,
+		Value: value,
+	}
+	if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+		if err := memcache.Set(c, item); err != nil {
+			c.Errorf("error setting item: %v", err)
+		}
+	} else if err != nil {
+		c.Errorf("error adding item: %v", err)
+	}
 }
 
 func postFetchMetaHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	var posts []Post
-	_, err := datastore.NewQuery(postKind).Order("-Date").GetAll(c, &posts)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+
+	var value []byte
+	// Get the item from the memcache
+	if item, err := memcache.Get(c, allPostKey); err == memcache.ErrCacheMiss {
+		c.Infof("info of all posts not in the cache")
+		var posts []Post
+		_, err := datastore.NewQuery(postKind).Order("-Date").GetAll(c, &posts)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		value, err = json.Marshal(posts)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Add the item to the memcache, if the key does not already exist
+		item := &memcache.Item{
+			Key:   allPostKey,
+			Value: value,
+		}
+		if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+			c.Infof("item with key %q already exists", item.Key)
+		} else if err != nil {
+			c.Errorf("error adding item: %v", err)
+		}
+	} else if err != nil {
+		c.Errorf("error getting item: %v", err)
+	} else {
+		c.Infof("Hit the cache")
+		value = item.Value
 	}
-	b, err := json.Marshal(posts)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+
 	w.Header().Add("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, "%v", string(b))
+	fmt.Fprintf(w, "%v", string(value))
 }
 
 func postFetchContentHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	c.Infof("In post fetch")
 	vars := mux.Vars(r)
 	filename := vars["link"]
 
-	content, err := getPostContent(filename)
-	if err != nil {
-		c.Errorf("Read content from %v error: %v", filename, err.Error())
-		return
+	var content string
+	memcacheKey := postContentKeyBase + filename
+	// Get the item from the memcache
+	if item, err := memcache.Get(c, memcacheKey); err == memcache.ErrCacheMiss {
+		c.Infof("post content not in the cache")
+		content, err = getPostContent(filename)
+		if err != nil {
+			c.Errorf("Read content from %v error: %v", filename, err.Error())
+			return
+		}
+		// Add the item to the memcache, if the key does not already exist
+		item := &memcache.Item{
+			Key:   memcacheKey,
+			Value: []byte(content),
+		}
+		if err := memcache.Add(c, item); err == memcache.ErrNotStored {
+			c.Infof("item with key %q already exists", item.Key)
+		} else if err != nil {
+			c.Errorf("error adding item: %v", err)
+		}
+	} else if err != nil {
+		c.Errorf("error getting item: %v", err)
+	} else {
+		c.Infof("Hit the cache")
+		content = string(item.Value)
 	}
+
 	c.Infof("%v", content)
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	fmt.Fprint(w, content)
