b76a877a9eea3e619e9f799279894b776b49f7f4
Kun <likunarmstrong@gmail.com>
2014-11-26 22:06:56 -0700

    Redirect blog to blog/

--- frontpage/main.go
+++ frontpage/main.go
@@ -4,8 +4,9 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const (
@@ -15,6 +16,7 @@ const (
 func init() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", mainHandler).Methods("GET")
+	r.HandleFunc("/blog", redirectBlogHandler).Methods("GET")
 	http.Handle("/", r)
 }
 
@@ -22,4 +24,8 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("cache-control", "public")
 	//w.Header().Set("max-age", "7200")
 	http.ServeFile(w, r, "./web/main.html")
+}
+
+func redirectBlogHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, r.URL.Path+"/", http.StatusSeeOther)
 }
